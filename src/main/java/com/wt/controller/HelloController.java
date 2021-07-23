c04b0a2f68a30c2475a3b18d25ca29486a10c0e6
wutian_pro <224076@qq.com>
2021-07-23 09:04:33 +0800

    x项目经理第一次提交项目boot5

---
+++ src/main/java/com/wt/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.wt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("info")
+    public String info(){
+        return "hello Stringboot Info";
+    }
+}
